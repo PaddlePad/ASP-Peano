582816d0d39996a29d9cb445ac8d267e98a4b5c6
Attia <med3attia@live.fr>
2021-01-15 18:24:20 +0100

    SVG implementierung (function: drawSvg)

--- Implementierung/peano.c
+++ Implementierung/peano.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 void peano(unsigned degree, uint64_t* x, uint64_t* y);
 
 
@@ -28,5 +29,28 @@ char *pCh;
     
 
 return 0;
+}
+void drawSvg( int deg ,uint64_t* x, uint64_t* y ){
+ uint64_t* x1=x;
+ uint64_t* y1=y;
+    FILE *fp;
+
+   fp = fopen("/tmp/test.txt", "w+");
+
+   fprintf(fp, " <svg viewBox=""-10 -10 120 120"" xmlns=""http://www.w3.org/2000/svg"">\n");
+   fprintf(fp, "<polyline stroke=""black"" fill=""none""\n");
+   fprintf(fp, "points=");
+   for (int i=0 ; i<pow(3,deg),i++ ){
+      
+       fprintf(fp, "%d,%d ",x1,y1);
+       y1++;
+       x1++
+   }
+   
+   fprintf(fp, "/>\n");
+   fprintf(fp, "</svg>\n");
+   
+   //fputs("This is testing for fputs...\n", fp);
+   fclose(fp);
 }
 
