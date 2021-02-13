008a1eb075c4ae9ad18803a61a796b843596765a
ge97rod <ge97rod@mytum.de>
2021-02-13 11:02:51 +0100

    Zeitmessungen fürmo iterativ

--- PeanoMoWork/newversionPeano.c
+++ PeanoMoWork/newversionPeano.c
@@ -10,110 +10,110 @@ void reverseInPlace(u_int64_t *x, u_int64_t *y, u_int64_t pos, u_int64_t size)
 
     for (u_int64_t i = 1; i < size; i++)
     {
-        u_int64_t tempy=y[i]-y[i-1];// direction left=-1 / right=1
-        u_int64_t tempx=x[i]-x[i-1];// direction up=1 / down =-1
-            y[pos + i] = y[pos + i-1] - tempy ;
-            x[pos + i] = x[pos + i-1] - tempx ;
+        u_int64_t tempy = y[i] - y[i - 1]; // direction left=-1 / right=1
+        u_int64_t tempx = x[i] - x[i - 1]; // direction up=1 / down =-1
+        y[pos + i] = y[pos + i - 1] - tempy;
+        x[pos + i] = x[pos + i - 1] - tempx;
     }
 }
 
-void mirrorInPlace(u_int64_t *x, u_int64_t*y, u_int64_t pos, u_int64_t size)
+void mirrorInPlace(u_int64_t *x, u_int64_t *y, u_int64_t pos, u_int64_t size)
 {
     for (u_int64_t i = 1; i < size; i++)
     {
-        u_int64_t tempy=y[i]-y[i-1];// direction left=-1 / right=1
-        u_int64_t tempx=x[i]-x[i-1];// direction up=1 / down =-1        
-            y[pos + i] = y[pos + i-1] - tempy ;
-            x[pos + i] = x[pos + i-1] + tempx ;
+        u_int64_t tempy = y[i] - y[i - 1]; // direction left=-1 / right=1
+        u_int64_t tempx = x[i] - x[i - 1]; // direction up=1 / down =-1
+        y[pos + i] = y[pos + i - 1] - tempy;
+        x[pos + i] = x[pos + i - 1] + tempx;
     }
 }
 
-void copyInPlace(u_int64_t *x,u_int64_t*y, u_int64_t pos, u_int64_t size)
+void copyInPlace(u_int64_t *x, u_int64_t *y, u_int64_t pos, u_int64_t size)
 {
     for (u_int64_t i = 1; i < size; i++)
     {
-        u_int64_t tempy=y[i]-y[i-1];// direction left=-1 / right=1
-        u_int64_t tempx=x[i]-x[i-1];// direction up=1 / down =-1
-            y[pos + i] = y[pos + i-1] + tempy ;
-            x[pos + i] = x[pos + i-1] + tempx ;
+        u_int64_t tempy = y[i] - y[i - 1]; // direction left=-1 / right=1
+        u_int64_t tempx = x[i] - x[i - 1]; // direction up=1 / down =-1
+        y[pos + i] = y[pos + i - 1] + tempy;
+        x[pos + i] = x[pos + i - 1] + tempx;
     }
 }
 
-void reverseMirrorInPlace(u_int64_t *x,u_int64_t*y,  u_int64_t pos, u_int64_t size)
+void reverseMirrorInPlace(u_int64_t *x, u_int64_t *y, u_int64_t pos, u_int64_t size)
 {
     for (u_int64_t i = 1; i < size; i++)
     {
-        u_int64_t tempy=y[i]-y[i-1];// direction left=-1 / right=1
-        u_int64_t tempx=x[i]-x[i-1];// direction up=1 / down =-1
-            y[pos + i] = y[pos + i-1] + tempy ;
-            x[pos + i] = x[pos + i-1] - tempx ;
-        }
+        u_int64_t tempy = y[i] - y[i - 1]; // direction left=-1 / right=1
+        u_int64_t tempx = x[i] - x[i - 1]; // direction up=1 / down =-1
+        y[pos + i] = y[pos + i - 1] + tempy;
+        x[pos + i] = x[pos + i - 1] - tempx;
+    }
 }
-int calcNextInplace(int currGrad, u_int64_t *x,u_int64_t*y, u_int64_t pos)
+int calcNextInplace(int currGrad, u_int64_t *x, u_int64_t *y, u_int64_t pos)
 {
     u_int64_t size = (u_int64_t)pow(3, 2 * currGrad);
     // Erster Zwischenstep
-    x[pos] = x[pos-1];
-    y[pos]=y[pos-1]+1;
+    x[pos] = x[pos - 1];
+    y[pos] = y[pos - 1] + 1;
 
     // Zweiter step
-    reverseMirrorInPlace(x,y, pos, size);
+    reverseMirrorInPlace(x, y, pos, size);
     pos += size;
 
     //Zwischenschritt nach oben
-    x[pos] = x[pos-1];
-    y[pos]=y[pos-1]+1;
+    x[pos] = x[pos - 1];
+    y[pos] = y[pos - 1] + 1;
 
     // Dritter step
-    copyInPlace(x,y, pos, size);
+    copyInPlace(x, y, pos, size);
     pos += size;
 
     //Zwischenschritt nach rechts
-    x[pos] = x[pos-1]+1;
-    y[pos]=y[pos-1];
+    x[pos] = x[pos - 1] + 1;
+    y[pos] = y[pos - 1];
 
     //Vierter Step
-    mirrorInPlace(x,y, pos, size);
+    mirrorInPlace(x, y, pos, size);
     pos += size;
 
     //Zwischenschritt nach unten
-    x[pos] = x[pos-1];
-    y[pos]=y[pos-1]-1;
+    x[pos] = x[pos - 1];
+    y[pos] = y[pos - 1] - 1;
 
     //Fünfter Step
-    reverseInPlace(x,y, pos, size);
+    reverseInPlace(x, y, pos, size);
     pos += size;
 
     //Zwischenschritt nach unten
-    x[pos] = x[pos-1];
-    y[pos]=y[pos-1]-1;
+    x[pos] = x[pos - 1];
+    y[pos] = y[pos - 1] - 1;
 
     //Sechster Step
-    mirrorInPlace(x,y, pos, size);
+    mirrorInPlace(x, y, pos, size);
     pos += size;
 
     //Zwischenschritt nach rechts
-    x[pos] = x[pos-1]+1;
-    y[pos]=y[pos-1];
+    x[pos] = x[pos - 1] + 1;
+    y[pos] = y[pos - 1];
 
     //Siebter Step
-    copyInPlace(x,y, pos, size);
+    copyInPlace(x, y, pos, size);
     pos += size;
 
     //Zwischenschritt nach oben
-    x[pos] = x[pos-1];
-    y[pos]=y[pos-1]+1;
+    x[pos] = x[pos - 1];
+    y[pos] = y[pos - 1] + 1;
 
     //Achter Step
-    reverseMirrorInPlace(x,y, pos, size);
+    reverseMirrorInPlace(x, y, pos, size);
     pos += size;
 
     //Zwischenschritt nach oben
-    x[pos] = x[pos-1];
-    y[pos]=y[pos-1]+1;
+    x[pos] = x[pos - 1];
+    y[pos] = y[pos - 1] + 1;
 
     //Letzter Step
-    copyInPlace(x,y, pos, size);
+    copyInPlace(x, y, pos, size);
     pos += size;
     return pos;
 }
@@ -124,41 +124,40 @@ void peanoInPlace(unsigned grad, u_int64_t *x1, u_int64_t *y1)
         printf("Error number not valid !");
         return;
     }
-//array
-    x1[0]=1;
-    y1[0]=1;
+    //array
+    x1[0] = 1;
+    y1[0] = 1;
     //array[1]
-    x1[1]=1;
-    y1[1]=2;
+    x1[1] = 1;
+    y1[1] = 2;
     //array[2]
-    x1[2]=1;
-    y1[2]=3;
+    x1[2] = 1;
+    y1[2] = 3;
     //array[3]
-    x1[3]=2;
-    y1[3]=3;
+    x1[3] = 2;
+    y1[3] = 3;
     //array[4]
-    x1[4]=2;
-    y1[4]=2;
+    x1[4] = 2;
+    y1[4] = 2;
     //array[5]
-    x1[5]=2;
-    y1[5]=1;
+    x1[5] = 2;
+    y1[5] = 1;
     //array[6]
-    x1[6]=3;
-    y1[6]=1;
+    x1[6] = 3;
+    y1[6] = 1;
     //array[7]
-    x1[7]=3;
-    y1[7]=2;
+    x1[7] = 3;
+    y1[7] = 2;
     //array[8]
-    x1[8]=3;
-    y1[8]=3;
+    x1[8] = 3;
+    y1[8] = 3;
     unsigned pos = 9;
     unsigned currGrad = 1;
     while (currGrad < grad)
     {
-        pos = calcNextInplace(currGrad,x1,y1, pos);
+        pos = calcNextInplace(currGrad, x1, y1, pos);
         currGrad++;
     }
-
 }
 void printCoordinates(u_int64_t *x, u_int64_t *y, u_int64_t len)
 {
@@ -202,15 +201,14 @@ int main(int argc, char *argv[])
 
     char *pCh;
     unsigned deg = 42;
-       struct timespec before;
+    struct timespec before;
     struct timespec after;
-   
+
     deg = strtoul(argv[1], &pCh, 10);
-    
+
     int size = (int)pow(9, deg);
     u_int64_t *x = (u_int64_t *)malloc(size * sizeof(u_int64_t));
     u_int64_t *y = (u_int64_t *)malloc(size * sizeof(u_int64_t));
-    
 
     if (x == NULL || y == NULL)
     {
@@ -218,25 +216,28 @@ int main(int argc, char *argv[])
         return (3);
     }
 
-  
     clock_gettime(CLOCK_MONOTONIC, &before);
-        peanoInPlace(deg, x,y);
-         
-        clock_gettime(CLOCK_MONOTONIC, &after);
- drawSvg(x,y,size);
-        // Je nach Angabeparameter wird eine SVG Datei erstellt
+    peanoInPlace(deg, x, y);
+    clock_gettime(CLOCK_MONOTONIC, &after);
+
+    printf("Peano calculation successfully terminated");
+    if (deg > 7) // Upwards from degree 7 we use seconds to avoid overflows
+    {
+        double secBefore = before.tv_sec + 1e-9 * before.tv_nsec;
+        double secAfter = after.tv_sec + 1e-9 * after.tv_nsec;
+        printf(" after %f seconds", secAfter - secBefore);
+    }
+    else
+        printf(" after %ld nanoseconds", (u_int64_t)after.tv_nsec - (u_int64_t)before.tv_nsec);
+
+    puts(".");
+
+   
+    //drawSvg(x, y, size);
     
-
-        // Je nach Angabe wird die Zeit ausgegeben
-        printf("Oparation successfully terminated");
-                                                       // Je nach Angabe wird die Zeit ausgegeben
-            printf(" after %ld nanoseconds", (u_int64_t)after.tv_nsec - (u_int64_t)before.tv_nsec);
-
-        puts(".");
 
     free(x);
     free(y);
 
     return 0;
 }
-
